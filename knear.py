235de8f9e2b73507d5b1ff94d48d1d8b1344ceeb
shalu99v@gmail.com <shalu99v@gmail.com>
2022-03-02 10:52:20 +0530

    knearest

--- knear.py
+++ knear.py
@@ -1,9 +1,11 @@
-from sklearn.neighbours  import KNeighboursclassifier
+from sklearn.neighbors  import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
 irisData=load_iris()
+print(irisData.data)
 x=irisData.data
 y=irisData.target
+print(y)
 x_train,x_test,y_train,y_test=(train_test_split(x,y,testsize=0.2,random_state=42))
 Knn=KNeighboursclassifier(n_neighbour=7)
 Knn.fit(x_train,y_train)
--- knearest.py
+++ knearest.py
@@ -4,9 +4,7 @@ from sklearn.datasets import load_iris
 irisData=load_iris()
 x=irisData.data
 y=irisData.target
-#split into traing of test data
 x_train,x_test,y_train,y_test=(train_test_split(x,y,testsize=0.2,random_state=42))
 Knn=KNeighboursclassifier(n_neighbour=7)
 Knn.fit(x_train,y_train)
-#prediction
 print(Knn.predict(x_test))
